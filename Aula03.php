978eaa6510832711c607422a30abeccc2a6e14a6
Everton Figueiredo <jose.everton.f@hotmail.com>
2020-09-09 15:28:59 -0300

    Aula 03 add

---
+++ Aula03.php
@@ -0,0 +1,30 @@
+<?php 
+    date_default_timezone_set('America/Sao_Paulo');
+    abstract class BancoDados{
+
+        const host = 'localhost';
+        const dbname = 'aula';
+        const user = 'root';
+        const password = '';
+
+        static function conectar(){
+            try {
+                $pdo = new PDO("mysql:
+                host=".self::host.";
+                dbname=".self::dbname.";
+                charset=utf8",
+                self::user,
+                self::password);
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                return $pdo;
+            } catch (PDOException $e) {
+                echo 'Error: '. $e->getMessage();
+            }
+        }
+    }
+
+    $conexao = BancoDados::conectar();  
+
+    if($conexao){
+        echo "Conectado";
+    }
